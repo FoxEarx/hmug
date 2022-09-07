93accc646277d88f8f0bbf8fe090fa30b2f3a5aa
ForEars <1228456740@qq.com>
2022-09-07 09:09:04 +0800

    对比

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -12,29 +12,30 @@
   export default {
     data() {
       return {
-        
+
       };
     }
   }
 </script>
 
 <style lang="scss">
-.topSearch {
-  width: 750rpx;
-  height: 70px;
-  background-color: #c00000;
-  display: flex;
-  justify-content: center;
-  overflow: hidden;
-  .search_btn {
-    width: 400rpx;
-    height: 30px;
-    margin-top: 30px;
-    background-color: #fff;
-    border-radius: 10px;
+  .topSearch {
+    width: 750rpx;
+    height: 70px;
+    background-color: #c00000;
     display: flex;
     justify-content: center;
-    align-items: center;
+    overflow: hidden;
+
+    .search_btn {
+      width: 400rpx;
+      height: 30px;
+      margin-top: 30px;
+      background-color: #fff;
+      border-radius: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   }
-}
 </style>
